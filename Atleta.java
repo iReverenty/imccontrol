6820bcd72127faeb5f448d93714ed671de74aecf
Roger V Lima <rogervieiralima61@gmail.com>
2020-08-27 23:48:47 +0000

    Initial commit

---
+++ Atleta.java
@@ -0,0 +1,21 @@
+class Atleta {
+ private String nome;
+ private int idade;
+
+ public void setNome(String n){
+   nome = n;
+  }
+
+ public void setIdade(int i){
+  idade = i;
+  }
+
+ public String getNome(){
+   return nome;
+  }
+
+ public int getIdade(){
+   return idade;
+  }
+
+}
---
+++ TesteSimples.java
@@ -0,0 +1,14 @@
+class TesteSimples{
+
+  public void executar(){
+
+    Atleta atleta = new Atleta();
+    atleta.setNome("Rev");
+    atleta.setIdade(25);
+
+    System.out.println("Nome = "+ atleta.getNome());
+    System.out.println("Idade = "+ atleta.getIdade());
+
+  }
+
+}
