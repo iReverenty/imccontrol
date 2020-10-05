24c67322cefc32e7229575e37e3aebd8031ecf0c
Roger V Lima <rogervieiralima61@gmail.com>
2020-10-05 23:09:13 +0000

    correção de código

--- Main.java
+++ Main.java
@@ -1,11 +1,18 @@
 class Main {
-  
-  public static void main(String[] args) {
+  public static void main(String[] args) throws Exception {
     
-    TesteSimples teste = new TesteSimples();
-    teste.executar();
+
+    //TesteSimples teste = new TesteSimples();
+    TestUnitario tu = new TestUnitario();  
+    tu.executarAll();
+
+    TelaConsoleMenu tela = new TelaConsoleMenu();
+    tela.executar();
+
+
+
+
     
-    
-    System.out.println("Hello world!");
+    System.out.println("\nimcmonitor, fim!");
   }
 }
---
+++ test/TestUnitario.java
@@ -0,0 +1,11 @@
+class TestUnitario {
+
+  public void executarAll() throws Exception {
+    
+    TestAtleta testAtleta = new TestAtleta();
+    testAtleta.testNome();
+    testAtleta.testIdade();
+
+  }
+
+}
